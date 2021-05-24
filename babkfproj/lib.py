b71103258b68077a6e60ccee4ecb1573f357b345
Georgios Alexandros Christodoulou <bab_alt@hotmail.com>
2021-05-24 12:13:35 +0200

    Initial Push

---
+++ babkfproj/lib.py
@@ -0,0 +1,7 @@
+def try_me():
+    from datetime import datetime
+    now = datetime.now()
+    current_time = now.strftime('%H:%M:%S')
+
+    return f"Hey i'm Alex, the time right now is {current_time} and i still need more time to be creative :D"
+
